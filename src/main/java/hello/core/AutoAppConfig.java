bece04d8f200e57b013c3f54a525d0fb827a14b0
Lucky-BE <byungeun96@gmail.com>
2022-10-09 16:11:24 +0900

    스프링 기본편 3주차
    - Singleton
    - ComponentScan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        //basePackages 를 지정하지 않으면 ComponentScan 설정 정보 클래스의 파일이 있는 위치가 시작위치가 됨
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
